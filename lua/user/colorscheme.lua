8317e206f450f9f7a4fbb77a72cae7813ab9a1dc
Leo Mendoza Jr <dev@skyturtle.io>
2022-10-17 17:21:47 -0700

    change to material colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,8 @@
-local colorscheme = "tokyonight-night"
+local colorscheme = "material"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
+vim.g.material_terminal_italics = 1
