693de691c5836da62fb1ceaeb206bceaa88ece35
Leo Mendoza Jr <dev@skyturtle.io>
2022-12-05 10:41:27 -0800

    add vim to treesitter parser

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -10,11 +10,21 @@ end
 
 configs.setup({
   -- put the languages you want in this table
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "svelte", "tsx", "typescript", "javascript"}, 
+  ensure_installed = {
+    "bash",
+    "javascript",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "svelte",
+    "tsx",
+    "typescript",
+    "vim"
+  },
   -- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  
   highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "css" }, -- list of language that will be disabled
