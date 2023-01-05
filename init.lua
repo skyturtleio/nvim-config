1d859e7798fe5a972d6273b70c15cac8328d8417
Leo Mendoza Jr <dev@skyturtle.io>
2023-01-05 10:25:17 -0800

    move keyamps into separate module

--- init.lua
+++ init.lua
@@ -5,68 +5,7 @@ vim.g.loaded_netrwPlugin = 1
 
 require("skyturtle.packer")
 require("skyturtle.options")
-
-
--- [[ Basic Keymaps ]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
-})
-
--- Shorten function name
-local keymap = vim.keymap.set
--- Silent keymap option
-local opts = { silent = true }
-
--- Press jj fast to escape
-keymap("i", "jj", "<ESC>", opts)
--- [[ Modes ]]
---    normal_mode = "n",
---    insert_mode = "i",
---    visual_mode = "v",
---    visual_block_mode = "x",
---    term_mode = "t",
---    command_mode = "c",
-
--- [[ Keys ]]
---   ctrl - example below is ctrl + h
---      keymap("n", "<C-h>", "<C-w>h", opts)
---   shift - example below is shift + l
---      keymap("n", "<S-l>", ":bnext<CR>", opts)
-
--- Normal Mode --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
--- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
-
+require("skyturtle.keymaps")
 
 -- Set lualine as statusline
 -- See `:help lualine.txt`
@@ -356,7 +295,7 @@ cmp.setup({
 -- NvimTree
 require("nvim-tree").setup()
 -- Togggle NvimTree
-keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {silent = true, desc = "File [E]xplorer"})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
