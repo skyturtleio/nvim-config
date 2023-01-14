d6823bb0160b1c0d7a0bf2a23a5d326d02480c01
Leo Mendoza Jr <dev@skyturtle.io>
2023-01-14 15:29:31 -0500

    add eex and heex tresitter parsers

--- lua/skyturtle/treesitter.lua
+++ lua/skyturtle/treesitter.lua
@@ -4,6 +4,8 @@ require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
 	ensure_installed = {
 		"elixir",
+		"eex",
+		"heex",
 		"go",
 		"javascript",
 		"lua",
