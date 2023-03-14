d20f0f23b91838e44f77ba470df0e9dcc68cc66b
Leo Mendoza Jr <dev@skyturtle.io>
2023-03-14 10:33:06 -0700

    change formatting to sync, use all buffers for null-ls

--- lua/skyturtle/lsp.lua
+++ lua/skyturtle/lsp.lua
@@ -167,7 +167,14 @@ cmp.setup({
 		}),
 	},
 	sources = {
-		{ name = "buffer" },
+		{
+			name = "buffer",
+			option = {
+				get_bufnrs = function()
+					return vim.api.nvim_list_bufs()
+				end,
+			},
+		},
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
 	},
