19381a0d1b405b254b45e0f98deabc01029ede22
Leo Mendoza Jr <dev@skyturtle.io>
2023-04-20 17:52:18 -0700

    change default window width for nvim-tree

--- lua/skyturtle/nvimtree.lua
+++ lua/skyturtle/nvimtree.lua
@@ -1,4 +1,8 @@
 -- NvimTree
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	view = {
+		width = 45,
+	},
+})
 -- Togggle NvimTree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true, desc = "File [E]xplorer" })
